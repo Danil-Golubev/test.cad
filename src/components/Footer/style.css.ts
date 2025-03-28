02bb2cfda24bebbaee3dbc7f227f7157176e9f66
Danil-Golubev <dokers190rus@mail.ru>
2025-03-28 22:47:03 +0300

    contact page height fix

--- src/components/Footer/style.css.ts
+++ src/components/Footer/style.css.ts
@@ -9,5 +9,4 @@ export const footerBlock = style({
   backgroundColor: "white",
   borderTopLeftRadius: "50px",
   borderTopRightRadius: "50px",
-  boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.08)",
 });
--- src/pages/Contact/style.css.ts
+++ src/pages/Contact/style.css.ts
@@ -5,6 +5,7 @@ export const container = style({
   padding: "20px",
   display: "flex",
   width: "100%",
+  height: "80vh",
   justifyContent: "center",
   alignItems: "center",
 });
